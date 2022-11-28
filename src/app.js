f647dab3226800485b5192d17d6a935ac19b68e9
André Correia <andrecorreiati@gmail.com>
2022-11-28 16:03:25 -0300

    feat: create authentication in routes

--- src/app.js
+++ src/app.js
@@ -6,9 +6,11 @@ import { errorHandling } from './utils/error.js';
 import routesAlunos from './routes/routesAlunos.js';
 import routesInstrutores from './routes/routesInstrutor.js';
 import routesFichas from './routes/routesFichas.js';
+import routeAuth from './routes/authRoutes.js'
 import routesGruposMusculares from './routes/routesGruposMusculares.js';
 import routesTiposExerxixios from './routes/routesTiposExercicios.js';
 import connectDatabase from './database/database.js';
+import { verificarToken } from './utils/verificarToken.js';
 
 const app = express();
 
@@ -23,10 +25,11 @@ app.use(
 );
 connectDatabase();
 
-app.use('/api/alunos', routesAlunos);
-app.use('/api/instrutores', routesInstrutores);
-app.use('/api/fichas', routesFichas);
-app.use('/api/gruposmusculares', routesGruposMusculares);
-app.use('/api/tipoexercicios', routesTiposExerxixios);
+app.use("/api/auth", routeAuth);
+app.use('/api/alunos', verificarToken, routesAlunos);
+app.use('/api/instrutores', verificarToken, routesInstrutores);
+app.use('/api/fichas', verificarToken, routesFichas);
+app.use('/api/gruposmusculares', verificarToken, routesGruposMusculares);
+app.use('/api/tipoexercicios', verificarToken, routesTiposExerxixios);
 
 export default app;
