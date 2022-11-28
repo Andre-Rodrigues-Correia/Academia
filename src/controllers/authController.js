b7b74391ad3fe57a6ba319179a1b5f1779e0dc14
William DS <williamds.uchiha@gmail.com>
2022-11-28 16:31:45 -0300

    Added Instructor Authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,28 +1,63 @@
-import Aluno from "../models/Aluno.js";
-import bcrypt from "bcryptjs";
-import { createError } from "../utils/error.js";
-import jwt from "jsonwebtoken";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import Aluno from '../models/Aluno.js';
+import Instrutor from '../models/Instrutor.js';
+import { createError } from '../utils/error.js';
 
-export const login = async (req, res, next) => {
-    try {
-        const aluno = await Aluno.findOne({ email: req.body.email });
-        if (!aluno) {
-            return next(createError(404, "Aluno não encontrado."))
-        };
-        const senhaValida = await bcrypt.compare(req.body.senha, aluno.senha);
-        if (!senhaValida) {
-            return next(createError(401, "Senha inválida."))
-        }
-        const { senha, ...dados } = aluno._doc;
-        const accessToken = jwt.sign({ id: aluno._id, admin: aluno.ativo },
-            process.env.JWT_SECRET, { expiresIn: "1h" });
-        res.cookie("accessToken", accessToken,
-            { httpOnly: true }).status(200).json(dados);
-
-        res.status(200).json(dados);
-    } catch (error) {
-        next(error);
+const loginAluno = async (req, res, next) => {
+  try {
+    const aluno = await Aluno.findOne({ email: req.body.email });
+    if (!aluno) {
+      return next(createError(404, 'Aluno não encontrado.'));
     }
+    const senhaValida = await bcrypt.compare(req.body.senha, aluno.senha);
+    if (!senhaValida) {
+      return next(createError(401, 'Senha inválida.'));
+    }
+    const { senha, ...dados } = aluno._doc;
+    const accessToken = jwt.sign(
+      { id: aluno._id, admin: aluno.ativo },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' },
+    );
+    res.cookie(
+      'accessToken',
+      accessToken,
+      { httpOnly: true },
+    ).status(200).json(dados);
+
+    res.status(200).json(dados);
+  } catch (error) {
+    next(error);
+  }
 };
 
+const loginInstrutor = async (req, res, next) => {
+  try {
+    const instrutor = await Instrutor.findOne({ email: req.body.email });
+    if (!instrutor) {
+      return next(createError(404, 'Instrutor não encontrado.'));
+    }
+    const senhaValida = await bcrypt.compare(req.body.senha, instrutor.senha);
+    if (!senhaValida) {
+      return next(createError(401, 'Senha inválida.'));
+    }
+    const { senha, ...dados } = instrutor._doc;
+    const accessToken = jwt.sign(
+      { id: instrutor._id, admin: instrutor.ativo },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' },
+    );
+    res.cookie(
+      'accessToken',
+      accessToken,
+      { httpOnly: true },
+    ).status(200).json(dados);
 
+    res.status(200).json(dados);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { loginAluno, loginInstrutor };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,19 @@
-import express from "express";
-import { login } from "../controllers/authController.js";
-import { verificarToken, verificarUsuario, verificarAdmin } from "../utils/verificarToken.js";
-    const router = express.Router();
-    router.post("/login", login);
-    router.get("/verificartoken", verificarToken, (req, res) => {
-        res.status(200).json({ message: "Você está autenticado." });
-    });
-    router.get("/verificarusuario/:id", verificarUsuario, (req, res) => {
-        res.status(200).json(
-            { message: "O id passado é do usuário logado ou de um administrador." });
-    });
-    router.get("/verificaradmin/:id", verificarAdmin, (req, res) => {
-        res.status(200).json({ message: "O id passado é de um administrador." });
-    });
+import express from 'express';
+import { loginInstrutor, loginAluno } from '../controllers/authController.js';
+import { verificarToken, verificarUsuario, verificarAdmin } from '../utils/verificarToken.js';
+
+const router = express.Router();
+router.post('/instrutor/login', loginInstrutor);
+router.post('/aluno/login', loginAluno);
+router.get('/verificartoken', verificarToken, (req, res) => {
+  res.status(200).json({ message: 'Você está autenticado.' });
+});
+router.get('/verificarusuario/:id', verificarUsuario, (req, res) => {
+  res.status(200).json(
+    { message: 'O id passado é do usuário logado ou de um administrador.' },
+  );
+});
+router.get('/verificaradmin/:id', verificarAdmin, (req, res) => {
+  res.status(200).json({ message: 'O id passado é de um administrador.' });
+});
 export default router;
